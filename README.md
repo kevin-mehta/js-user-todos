fd0c91ab31608ac38690ac76e57a717b8e4b5440
kevin-mehta <kevin.h.mehta@gmail.com>
2024-05-01 14:02:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-user-todos
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kevin-mehta/js-user-todos)
---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('Hello!');
+
+/**
+ * Given an API returning a list of todos, we want to fetch the list,
+ * create a separate block for each user, and display their todos in the
+ * appropriate block.
+ *
+ * Use this endpoint URL to get the todos:
+ * https://dummyjson.com/todos?limit=10&skip=80.
+ *
+ * It will return the following structure with total of 10 todos:
+ * {
+ *  "todos": [
+ *    {
+ *      "id": 1,
+ *      "todo": "Do something nice for someone I care about",
+ *      "completed": true,
+ *      "userId": 26
+ *    }
+ *  ]
+ * }
+ */
