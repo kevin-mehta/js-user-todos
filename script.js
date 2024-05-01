beee5cc1bf59209320d830777eb3e1ac1745b65e
kevin-mehta <kevin.h.mehta@gmail.com>
2024-05-01 14:39:57 +0530

    Generated user todos

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Hello!');
-
 /**
  * Given an API returning a list of todos, we want to fetch the list,
  * create a separate block for each user, and display their todos in the
@@ -20,3 +18,21 @@ console.log('Hello!');
  *  ]
  * }
  */
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('DOMContentLoaded!');
+
+    let eleRoot = document.getElementById("root");
+    let strUrlGetUserTodos = 'https://dummyjson.com/todos?limit=10&skip=80';
+    fetch(strUrlGetUserTodos)
+        .then(response => response.json())
+        .then(result => {
+            console.log("result: ", result);
+            let arrTodos = result.todos;
+            let strGeneratedHtml = '';
+            for (let index = 0; index < arrTodos.length; index++) {
+                strGeneratedHtml += "<div class='todo'><strong>User # " + arrTodos[index].id + "</strong><br/> <span>" + arrTodos[index].todo + "</span></div>";
+            }
+            eleRoot.innerHTML = strGeneratedHtml;
+        });
+});
